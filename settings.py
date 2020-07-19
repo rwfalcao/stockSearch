1b06610829b2d8ed3033918d5eec468d974c3837
Rodrigo Wehbe <falcaorw@gmail.com>
2020-07-19 14:17:39 -0300

    #2: Model for stocks

--- settings.py
+++ settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'b2blue.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR + '/symbol_search', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- symbol_search/models.py
+++ symbol_search/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Stock(models.Model):
+
+    symbol = models.CharField(
+        'Symbol',
+        max_length=10,
+        null=False
+        )
+
+    name = models.CharField(
+        'Description',
+        max_length=200,
+        null=False
+        )
+
+    last_sale = models.DecimalField(
+        'Last sale value'
+        max_digits=12, 
+        decimal_places=2
+        )
+
+    market_cap = models.DecimalField(
+        'Market Cap'
+        max_digits=12, 
+        decimal_places=2
+        )
+
+    ipo_year = models.IntegerField(
+        'IPO Year'
+        max_value=3000
+        )
+
+    nasdaq_url models.URLField()
+
